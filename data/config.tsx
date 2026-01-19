6d3696b0591d6f14179c29ff8955768d1f5e7a6d
zahidahmadusmani <zahidahmadusmani@gmail.com>
2026-01-19 12:55:08 +0500

    Fixed react-icons

--- data/config.tsx
+++ data/config.tsx
@@ -6,8 +6,7 @@ import {
   FaLinkedin,
   FaYoutube,
 } from "react-icons/fa";
-// import { SiUpwork } from "react-icons/si";
-import { FaSquareUpwork } from "react-icons/fa6";
+import { SiUpwork } from "react-icons/si";
 import { FiCheck } from "react-icons/fi";
 import { Logo } from "./logo";
 
@@ -92,7 +91,7 @@ const siteConfig = {
       },
       {
         href: "https://www.upwork.com/agencies/techemulsion/",
-        label: <FaSquareUpwork size="20" aria-label="Upwork" title="Upwork" />,
+        label: <SiUpwork size="20" aria-label="Upwork" title="Upwork" />,
       },
     ],
   },

cdfd32c6934978885fe338c2d8770abd53bd767d
zahidahmadusmani <zahidahmadusmani@gmail.com>
2026-02-25 16:00:15 +0500

    Updated the contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -7,7 +7,6 @@ import {
   Icon,
   Text,
   useColorModeValue,
-  VStack,
 } from "@chakra-ui/react";
 import { ButtonLink } from "components/button-link";
 import Contact from "components/Contact";
@@ -21,6 +20,7 @@ import { FaChevronRight } from "react-icons/fa";
 const Contacts = () => {
   const headingColor = useColorModeValue("gray.800", "white");
   const textColor = useColorModeValue("gray.600", "gray.100");
+  const dividerColor = useColorModeValue("gray.200", "gray.600");
   return (
     <Box id="services">
       <Head>
@@ -83,34 +83,26 @@ const Contacts = () => {
 
   gtag('config', 'G-DJFC9CERLF')`}
       </Script>
-      <Container maxW="container.xl" py="20">
-        <BackgroundGradient height="100%" zIndex="-1" />
+      <Box position="relative" minH="100vh" color={headingColor}>
+        {/* Full-page gradient - same as portfolio, blog, services, careers */}
         <Box
-          display={{ base: "block", md: "flex" }}
-          px="15"
-          mt={10}
-          justifyContent={"space-between"}
-          mb={10}>
-          <Box>
-            <Heading as="h2" size="lg">
-              Contact Page
-            </Heading>
-            <Text
-              color={textColor}
-              fontSize="lg"
-              mt="4"
-              width={["auto", null, "80%"]}>
-              Whether you&apos;re looking for innovative software solutions,
-              need to scale your development team, or want to discuss your next
-              big project, we&apos;d love to hear from you! At Tech Emulsion, we
-              are dedicated to delivering exceptional results in web
-              development, SaaS solutions, Chrome extensions, QA testing, cloud
-              services, and artificial intelligence.
-            </Text>
-          </Box>
+          position="absolute"
+          top={0}
+          left={0}
+          right={0}
+          bottom={0}
+          zIndex={-1}
+          overflow="hidden"
+          pointerEvents="none"
+        >
+          <BackgroundGradient height="100%" width="100%" />
+        </Box>
 
-          {/* Breadcrumb - matches portfolio style */}
-          <Flex justify="flex-end" mt="4">
+        {/* Top margin - clear fixed navbar */}
+        <Box pt={{ base: 20, md: 24 }} />
+        <Container maxW="container.xl" pt={6} pb={20} position="relative" zIndex={1}>
+          {/* Breadcrumb */}
+          <Flex justify="flex-end" mb={8}>
             <ButtonGroup
               sx={{
                 bg: "none",
@@ -137,9 +129,77 @@ const Contacts = () => {
               </Text>
             </ButtonGroup>
           </Flex>
-        </Box>
-        <Contact />
-      </Container>
+
+          {/* Header section - same layout as portfolio, blog, services, careers */}
+          <Box
+            minH={{ base: "280px", md: "35vh" }}
+            display={{ base: "block", md: "grid" }}
+            gridTemplateColumns={{ md: "1fr 1fr" }}
+            borderTopWidth="1px"
+            borderColor={dividerColor}
+            mx={-6}
+            px={6}
+          >
+            {/* Left: Heading - top aligned */}
+            <Box
+              py={8}
+              pr={{ base: 0, md: 6 }}
+              display="flex"
+              alignItems="flex-start"
+              borderRightWidth={{ md: "1px" }}
+              borderColor={dividerColor}
+              sx={{
+                borderColor: "gray.200 !important",
+                _dark: { borderColor: "gray.600 !important" },
+              }}
+            >
+              <Heading
+                as="h1"
+                fontSize={{ base: "4xl", md: "5xl", lg: "6xl" }}
+                fontWeight="bold"
+                color={headingColor}
+              >
+                Get in touch
+              </Heading>
+            </Box>
+            {/* Right: Description - bottom aligned */}
+            <Box
+              pl={{ base: 0, md: 6 }}
+              py={{ base: 4, md: 8 }}
+              display="flex"
+              alignItems={{ base: "flex-start", md: "flex-end" }}
+              justifyContent={{ base: "flex-start", md: "flex-end" }}
+            >
+              <Text
+                color={textColor}
+                fontSize="lg"
+                lineHeight="tall"
+                textAlign={{ base: "left", md: "left" }}
+                maxW={{ md: "420px" }}
+              >
+                Whether you&apos;re looking for innovative software solutions,
+                need to scale your development team, or want to discuss your next
+                big project, we&apos;d love to hear from you! At Tech Emulsion, we
+                are dedicated to delivering exceptional results in web
+                development, SaaS solutions, Chrome extensions, QA testing, cloud
+                services, and artificial intelligence.
+              </Text>
+            </Box>
+          </Box>
+
+          {/* Divider bar - matches portfolio, blog, services, careers */}
+          <Box
+            bg="transparent"
+            py={2}
+            px={6}
+            mx={-6}
+            borderTopWidth="1px"
+            borderColor={dividerColor}
+          />
+
+          <Contact />
+        </Container>
+      </Box>
     </Box>
   );
 };
